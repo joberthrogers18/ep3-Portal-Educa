f2c9b979ea2a3b8949c55c05ff76a5fd13438381
Elias Bernardo <ebmm01@gmail.com>
2017-12-04 21:12:17 -0200

    Somente usuários com permisão de admin podem acessar o painel de administração

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,12 +1,21 @@
 RailsAdmin.config do |config|
 
+  config.authorize_with do |controller|
+    
+    redirect_to main_app.root_path unless current_user.try(:admin?)
+
+  end
+    
+  
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+
+    config.authenticate_with do
+     warden.authenticate! scope: :user
+   end
+   config.current_user_method(&:current_user)
+
 
   ## == Cancan ==
   # config.authorize_with :cancan
