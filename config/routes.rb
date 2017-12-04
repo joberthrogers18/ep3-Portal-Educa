5a1fd272d4b395a665fd7357c16989334f56d1fc
Elias Bernardo <ebmm01@gmail.com>
2017-12-04 09:03:46 -0200

    Sistema de autenticação básico

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
+#root :to => 'users#index'
+resources :user_sessions
+resources :users
+
+get 'login' => 'user_sessions#new', :as => :login
+post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  resources :users
   resources :articles do
   resources :comments
 end
