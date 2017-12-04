e3f9216369a79186d29dbf749b6e8dc6ca42b519
Elias Bernardo <ebmm01@gmail.com>
2017-12-04 14:33:47 -0200

    Somente usuários logados podem criar, editar ou excluir artigos. Guest users podem apenas visualizá-los

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 include ArticlesHelper
-before_action :authenticate_user!
+#before_action :authenticate_user!
     def index
         @article = Article.all
     end
