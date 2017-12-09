ee3f5a2938ebaabe1ad70986fdab51526555748e
Elias Bernardo <ebmm01@gmail.com>
2017-12-08 22:53:10 -0200

    O perfil de usuários agora possui uma imagem que pode ser alterada pelo usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,6 @@ class User < ApplicationRecord
 
 end
 has_many :articles, foreign_key: :author_id
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
